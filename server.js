214dff2e36c827689dd843e646accec21413a620
KendryckAndrete <steve.ka957@gmail.com>
2025-08-27 14:43:18 -0300

    atualiza rotas no html e ajusta configuração do servidor

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("node:path");
 const router = require("./src/routes/index.js");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3333
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "src/public")))
 app.use("/", router)
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta http://localhost:3000");
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
 })
